66c81b890c36331816260b65e0de4b41b0e3246a
NEW <NEW@DESKTOP-OMQDSIF>
2022-09-07 23:47:03 +0300

    Add project files.

---
+++ DataAccess/Entities/YemekTarifi.cs
@@ -0,0 +1,20 @@
+﻿using AppCore.Records.Bases;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Entities
+{
+    public class YemekTarifi : RecordBase
+    {
+        [Required]
+        [StringLength(400)]
+        public string Adi { get; set; }
+
+        [StringLength(10000)]
+        public string Tarif { get; set; }
+
+        //public int MalzemeId { get; set; }
+
+        public string MalzemeAdi;
+        public Malzeme Malzemeler { get; set; }
+    }
+}
